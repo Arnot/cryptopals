468ba3dcdd41637d542dd17d17f91484d89abc60
Arno Tiemersma <arno.tiemersma@gmail.com>
2016-08-30 12:11:02 +0200

    Set1.2: XOR 2 buffers
    Moved main function into set1.c, built a basic menu
    Also split off utility functions into separate files

--- base64.c
+++ base64.c
@@ -1,3 +1,4 @@
+#include "cryptopals.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -59,47 +60,3 @@ void print_b64(char *bytes, size_t length) {
   printf("\n");
 }
 
-char *hex_to_bytes(char *input_string, unsigned int length) {
-  int i;
-
-  char *out = malloc(length);
-  char conv_string[3] = {0};
-
-  if (out == NULL) {
-    printf("Error allocating byte-array\n");
-    return NULL;
-  }
-
-  for (i = 0; i < length; i++) {
-    conv_string[0] = input_string[2*i];
-    conv_string[1] = input_string[2*i + 1];
-    out[i] = (char) strtol(conv_string, NULL, 16);
-  }
-
-  return out;
-}
-
-int main(void) {
-  size_t length;
-  ssize_t read;
-  ssize_t num_bytes;
-  char *input_string = NULL;
-  char *bytes;
-
-  while((read = getline(&input_string, &length, stdin)) > 1) {
-    /* Remove trailing newline */
-    read = read - 1;
-    input_string[read] = '\0';
-    /* Every 2 hex characters denote 1 byte */
-    /* Assume that all input is of even length */
-    num_bytes = read/2;
-
-    bytes = hex_to_bytes(input_string, read/2);
-    print_b64(bytes, read/2);
-  }
-
-  free(input_string);
-  free(bytes);
-
-  return 0;
-}
