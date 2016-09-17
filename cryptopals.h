a9ed88a114dc20e5d5fc5b9bc296333305c94f30
Arno Tiemersma <arno.tiemersma@gmail.com>
2016-09-17 12:16:40 +0200

    remove stuff from root folder

--- cryptopals.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CRYPTOPALS_H_
-#define _CRYPTOPALS_H_
-#include <stdlib.h>
-
-void print_b64(char *bytes, size_t length);
-char *hex_to_bytes(char *input_string, unsigned int length);
-void xor_buffers(char *buffer1, char *buffer2, size_t num_bytes);
-
-#endif
