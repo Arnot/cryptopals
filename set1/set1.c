9c99574c46ec00f5f3259d196337fd016b203e92
Arno Tiemersma <arno.tiemersma@gmail.com>
2016-11-04 16:03:08 +0100

    Set1: Solve challenge 8

--- set1/set1.c
+++ set1/set1.c
@@ -1,52 +1,54 @@
 #include "cryptopals.h"
+#include <openssl/conf.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
 
-void xor() {
-  size_t length1 = 0;
-  size_t length2 = 0;
-  ssize_t read1, read2;
-  ssize_t num_bytes;
-  char *input_string1 = NULL;
-  char *input_string2 = NULL;
-  char *bytes1, *bytes2;
-  char *result;
+void xor
+    () {
+      size_t length1 = 0;
+      size_t length2 = 0;
+      ssize_t read1, read2;
+      ssize_t num_bytes;
+      char *input_string1 = NULL;
+      char *input_string2 = NULL;
+      char *bytes1, *bytes2;
+      char *result;
 
-  scanf("\n");
+      scanf("\n");
 
-  read1 = getline(&input_string1, &length1, stdin);
-  read2 = getline(&input_string2, &length2, stdin);
+      read1 = getline(&input_string1, &length1, stdin);
+      read2 = getline(&input_string2, &length2, stdin);
 
-  if (read1 != read2) {
-    printf("Strings are not of equal length\n");
-    return;
-  }
+      if (read1 != read2) {
+        printf("Strings are not of equal length\n");
+        return;
+      }
 
-  num_bytes = read1 / 2;
+      num_bytes = read1 / 2;
 
-  bytes1 = hex_to_bytes(input_string1, num_bytes);
-  bytes2 = hex_to_bytes(input_string2, num_bytes);
+      bytes1 = hex_to_bytes(input_string1, num_bytes);
+      bytes2 = hex_to_bytes(input_string2, num_bytes);
 
-  result = xor_buffers(bytes1, bytes2, num_bytes);
+      result = xor_buffers(bytes1, bytes2, num_bytes);
 
-  printf("%s\n", result);
+      printf("%s\n", result);
 
-  free(bytes1);
-  free(bytes2);
-  free(input_string1);
-  free(input_string2);
-  free(result);
-}
+      free(bytes1);
+      free(bytes2);
+      free(input_string1);
+      free(input_string2);
+      free(result);
+    }
 
-void single_byte_xor() {
+    void single_byte_xor() {
   /* size_t length = 0; */
   ssize_t read;
   ssize_t num_bytes;
-  char *input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+  char *input =
+      "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
   char *bytes, *result;
   unsigned char key;
   float score;
@@ -77,7 +79,8 @@ void single_byte_xor() {
   printf("Top 5 keys with highest similarity to English: \n");
   for (int i = 0; i < 5; i++) {
     result = xor_single_char(bytes, num_bytes, scores[i].key);
-    printf("%d.\t key: %x; score = %f\t", i+1, scores[i].key, scores[i].score);
+    printf("%d.\t key: %x; score = %f\t", i + 1, scores[i].key,
+           scores[i].score);
     printf("%s\n", result);
 
     free(result);
@@ -87,7 +90,7 @@ void single_byte_xor() {
 }
 
 void find_single_byte_xor() {
-  FILE* fp;
+  FILE *fp;
   ssize_t read;
   ssize_t num_bytes;
   size_t length = 0;
@@ -109,8 +112,9 @@ void find_single_byte_xor() {
   }
 
   maxscore = -1;
-  while((read = getline(&input_string, &length, fp)) > 0 && line < 400) { /* 326 lines in 4.txt */
-    num_bytes = read/2;
+  while ((read = getline(&input_string, &length, fp)) > 0 &&
+         line < 400) { /* 326 lines in 4.txt */
+    num_bytes = read / 2;
     bytes = hex_to_bytes(input_string, num_bytes);
 
     /* Brute force key to find 'most english' text */
@@ -118,12 +122,13 @@ void find_single_byte_xor() {
       result = xor_single_char(bytes, num_bytes, key);
       score = english_similarity_score(result);
 
-      if (score > maxscore){
+      if (score > maxscore) {
         maxscore = score;
         bestline = line;
         free(best_match);
         best_match = result;
-        /* printf("Line %lu - English text found:\n%s\nscore: %f\n", line, best_match, maxscore); */
+        /* printf("Line %lu - English text found:\n%s\nscore: %f\n", line,
+         * best_match, maxscore); */
       } else {
         free(result);
       }
@@ -140,8 +145,9 @@ void find_single_byte_xor() {
   fclose(fp);
 }
 
-void repeating_xor(){
-  char string[] = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+void repeating_xor() {
+  char string[] = "Burning 'em, if you ain't quick and nimble\nI go crazy when "
+                  "I hear a cymbal";
   char key[] = "ICE";
   char *result;
 
@@ -177,14 +183,14 @@ void break_repeating_xor() {
 
   read = getline(&input_string, &length, fp);
 
-  num_bytes = strlen(input_string)/2;
+  num_bytes = strlen(input_string) / 2;
 
   bytes = hex_to_bytes(input_string, strlen(input_string));
 
   keysize = find_key_size(bytes, num_bytes);
 
   blocks = create_blocks(bytes, num_bytes, keysize);
-  num_blocks = num_bytes/keysize;
+  num_blocks = num_bytes / keysize;
 
   printf("Keysize: %d, number of blocks: %lu\n", keysize, num_blocks);
 
@@ -203,7 +209,7 @@ void break_repeating_xor() {
       result_key = xor_single_char(transposed_buffer, num_blocks, testkey);
       score = english_similarity_score(result_key);
 
-      if (score > maxscore){
+      if (score > maxscore) {
         maxscore = score;
         bestkey = testkey;
       }
@@ -245,7 +251,7 @@ void decrypt_aes_ecb() {
   }
   read = getline(&input_string, &length, fp);
 
-  num_bytes = strlen(input_string)/2;
+  num_bytes = strlen(input_string) / 2;
 
   bytes = hex_to_bytes(input_string, strlen(input_string));
 
@@ -270,7 +276,7 @@ void decrypt_aes_ecb() {
 
   EVP_DecryptFinal_ex(ctx, decrypted_text + decrypt_len, &decrypt_len);
 
-  printf("%s\n", (char*)decrypted_text);
+  printf("%s\n", (char *)decrypted_text);
 
   EVP_CIPHER_CTX_free(ctx);
   EVP_cleanup();
@@ -279,20 +285,76 @@ void decrypt_aes_ecb() {
   return;
 }
 
+void detect_aes_ecb() {
+  ssize_t read;
+  char *input_string = NULL;
+  size_t length = 0;
+  int i, j, k;
+  FILE *fp;
+  ssize_t num_bytes, num_blocks;
+  int num_same_blocks, max_same_blocks = 0;
+  int block_equal = 0, bestline = 0, line = 1;
+  char *bytes, **blocks;
+
+  fp = fopen("8.txt", "r");
+  if (fp == NULL) {
+    printf("Challenge 7: Could not open file!\n");
+    return;
+  }
+
+  while ((read = getline(&input_string, &length, fp)) > 0) {
+    num_bytes = strlen(input_string) / 2;
+    bytes = hex_to_bytes(input_string, num_bytes);
+    blocks = create_blocks(bytes, num_bytes, 16);
+    num_blocks = num_bytes / 16;
+    num_same_blocks = 0;
+
+    for (i = 0; i < num_blocks; i++) {
+      for (j = i+1; j < num_blocks; j++) {
+        block_equal = 1;
+
+        for (k = 0; k < 16; k++) {
+          if (blocks[i][k] != blocks[j][k]) {
+            block_equal = 0;
+          }
+        }
+
+        if (block_equal) {
+          num_same_blocks++;
+        }
+      }
+    }
+
+    if (num_same_blocks > max_same_blocks) {
+      max_same_blocks = num_same_blocks;
+      bestline = line;
+    }
+
+    line++;
+  }
+
+  printf("Best line: %d. Similar blocks: %d\n", bestline, max_same_blocks);
+
+  free(input_string);
+  free(bytes);
+  cleanup_blocks(blocks, num_blocks);
+}
+
 int main(void) {
   int command = 0;
 
   printf("Cryptopals Set 1\n");
   do {
     printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-    printf("1. hex to base64\n");
-    printf("2. xor of 2 buffers\n");
-    printf("3. brute-force single key xor\n");
-    printf("4. find most-English decrypted sentence\n");
-    printf("5. repeating-key xor\n");
-    printf("6. break repeating-key xor\n");
-    printf("7. decrypt AES in ECB mode\n");
-    printf("0. quit\n");
+    printf("1. Hex to base64\n");
+    printf("2. Xor of 2 buffers\n");
+    printf("3. Brute-force single key xor\n");
+    printf("4. Find most-English decrypted sentence\n");
+    printf("5. Repeating-key xor\n");
+    printf("6. Break repeating-key xor\n");
+    printf("7. Decrypt AES in ECB mode\n");
+    printf("8. Detect AES in ECB mode\n");
+    printf("0. Quit\n");
 
     if (scanf("%d", &command) == 0) {
       command = 0;
@@ -319,6 +381,9 @@ int main(void) {
       break;
     case 7:
       decrypt_aes_ecb();
+    case 8:
+      detect_aes_ecb();
+      break;
       break;
     default:
       printf("Bye!\n");
